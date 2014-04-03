a44b87ed1c332f8437240612d61dd928b860e193
Ray <leixuping@gmail.com>
2014-04-03 19:43:47 -0400

    Update SocketClientProtocolSignals.java

--- proj5/JSPHDClient_Tomcat/WEB-INF/src/net/SocketClientProtocolSignals.java
+++ proj5/JSPHDClient_Tomcat/WEB-INF/src/net/SocketClientProtocolSignals.java
@@ -6,17 +6,17 @@ public interface SocketClientProtocolSignals {
 	final String listeningForProperties = 	"Listen4Prop";
 	final String builtPropertiesSuccess =	"BuiltPropSuc";
 	final String builtPropertiesFailure =	"BltPropFail";
-	final String queryModelNames = 			"qModelNames";
+	final String queryModelNames = 		"qModelNames";
 	final String requestModelBackToClient = "getModel";
-	final String notFoundModel = 			"NotFoundMdl";
-	final String foundModel = 				"FoundModel";
-	final String listeningForModel = 		"Listen4Mdl";
-	final String updateModelRequest = 		"Models";
-	final String updateModelSuccess =		"UpdateSuc";
-	final String updateModelFailure =		"UpdateFail";
-	final String queryModelDetails = 		"qModelDetails";
-	final String queryOptionSetNames = 		"qOptionNames";
-	final String queryOptionNames = 		"qOptvalues";
-	final String updateModel = 				"updateModel";
+	final String notFoundModel = 		"NotFoundMdl";
+	final String foundModel = 		"FoundModel";
+	final String listeningForModel = 	"Listen4Mdl";
+	final String updateModelRequest = 	"Models";
+	final String updateModelSuccess =	"UpdateSuc";
+	final String updateModelFailure =	"UpdateFail";
+	final String queryModelDetails = 	"qModelDetails";
+	final String queryOptionSetNames = 	"qOptionNames";
+	final String queryOptionNames = 	"qOptvalues";
+	final String updateModel = 		"updateModel";
 
 }
